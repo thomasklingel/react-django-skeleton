7582747bffa8b49c402cd98829b3354d1df3e933
thomas <thomas.ks@live.fr>
2020-10-09 16:25:48 +0200

    React/Django hot reloads enabled

---
+++ react_django_skeleton/frontend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FrontenConfig(AppConfig):
+    name = 'frontend'
--- react_django_skeleton/react_django_skeleton/settings.py
+++ react_django_skeleton/react_django_skeleton/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpack_loader',
     'frontend'
 ]
 
@@ -119,3 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+#     os.path.join(BASE_DIR, "frontend/"),
+# )
+
+# Webpack
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'frontend/webpack-stats.dev.json'),
+    }
+}
